a7333c562d6959b572c8efb14cc63789ae9f31c1
PrettyAntony <145509389+PrettyAntony@users.noreply.github.com>
2024-03-04 12:33:41 -0500

    Add files via upload
    Lab1

---
+++ lab1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Greetings from "./Greetings.js";
+import React from 'react';
+
+function App() {
+  return (
+    <div className='App'>
+      <Greetings firstName="Pretty" />
+    </div>
+  );
+}
+
+export default App;
